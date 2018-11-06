d0eafa687fa40bb98dee1574edfdbfec14293971
Pooja Arshanapally <parshanapally@qca-sdc.com>
2018-11-06 15:56:45 -0600

    docs: install new versions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App/App'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import rootReducer from './reducers'
-import thunk from 'redux-thunk'
-import './index.css'
-import 'tachyons'
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import App from "./components/App/App";
+import React from "react";
+import ReactDOM from "react-dom";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import "./index.css";
+import "tachyons";
 
-const store = createStore(
-  rootReducer,
-  compose(	
-  	applyMiddleware(thunk)
-  )
-)
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
